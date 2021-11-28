e742a6a7bbaa79bd9e04a329f59fe68d3ba03369
Alex Qwxlea <qwxlea@gmail.com>
2021-11-28 14:10:01 +0200

    testing setup - added zshrc

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+BASEDIR=$HOME/Projects/dotfiles
+
+CONFIG="install.conf.yaml"
+DOTBOT_DIR="dotbot"
+
+DOTBOT_BIN="bin/dotbot"
+#BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#cd "${BASEDIR}"
+
+[ -d "$BASEDIR" ] || git clone git@github.com:QWxleA/dotfiles.git "$BASEDIR"
+cd "$BASEDIR"
+
+git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
+git submodule update --init --recursive "${DOTBOT_DIR}"
+
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
